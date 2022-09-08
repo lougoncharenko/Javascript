6d73a0cc443d9a3216b6ae4d764d150e4a0cff80
Louisa Goncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-09-08 09:15:04 -0400

    Add files via upload

---
+++ javascript/basics.js
@@ -0,0 +1,37 @@
+//console.log("Hello World");
+
+let username = "Louisa";
+
+let userID= 42;
+userID += 3.14;
+
+console.log(username);
+console.log(typeof(username));
+console.log(userID);
+console.log(typeof(userID));
+
+//string
+string="New york city";
+
+//number
+number = 45;
+
+//boolean
+boolean = "True";
+
+//operations
+console.log(5 + 7);
+
+console.log(6 - 2);
+
+console.log(3 * 5);
+
+console.log(10 / 5);
+
+console.log(22 % 7);
+
+//properties
+console.log("Codecademy".length);
+
+//built-in objects
+console.log(Math.ceil(43.8));
